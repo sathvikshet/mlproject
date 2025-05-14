78fc228dda12c85e0f3848870061cd76c8091c0b
sathviksshet <sathviksathvik798@gmail.com>
2025-05-14 17:04:56 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,9 @@
 import sys
-import logging
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.logger import logging
+import sys
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -20,6 +24,7 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+
 
     
     
